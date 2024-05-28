e076bb16308974cbafdbe4c0626bdc43863d9923
DoormatIka <trashcanartistlily@gmail.com>
2024-05-28 19:17:37 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace Main
+{
+	using System;
+	class Main
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+
+	// a class to manage multiple prints into one print.
+	class Merger
+	{
+		public List<char> current_frame;
+		public Merger() {
+			List<char> current_frame = List<char>();
+		}
+		// higher layer, more priority.
+		public void AddFrame(Frame frame)
+		{
+		}
+		public void Draw()
+		{
+
+		}
+	}
+
+	class Frame
+	{
+		public int layer;
+		public string frame;
+		public Coordinate coords;
+	}
+	class Coordinate
+	{
+		public int x;
+		public int y;
+	}
+}
